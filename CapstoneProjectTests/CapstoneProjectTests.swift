700d276d7ab8ba59c32c78e55f3e9c2a4def31c8
m-chamling <mangtoyarai@gmail.com>
2025-04-18 14:28:05 -0400

    Initial Commit

---
+++ CapstoneProjectTests/CapstoneProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectTests.swift
+//  CapstoneProjectTests
+//
+//  Created by Mangtoya Rai on 4/18/25.
+//
+
+import Testing
+@testable import CapstoneProject
+
+struct CapstoneProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
